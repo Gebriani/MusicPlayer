b5c49906a2e4da814787cf49a7c942f5c755d7ff
gebriani <gebriani@gmail.com>
2021-08-27 22:53:19 +0700

    change image size

--- app/src/main/java/com/orcaella/mymusicplayer/presentation/searchmusic/MusicCellClickListener.kt
+++ app/src/main/java/com/orcaella/mymusicplayer/presentation/searchmusic/MusicCellClickListener.kt
@@ -2,7 +2,4 @@ package com.orcaella.mymusicplayer.presentation.searchmusic
 
 interface MusicCellClickListener {
     fun onStartMusicClickListener(urlString: String)
-    fun onPauseMusicClickListener()
-    fun onBackwardMusicClickListener()
-    fun onForwardMusicClickListener()
 }
